7a450a46c517e0dc5143f8013b07b58555d0512d
StXh <stxh007@gmail.com>
2024-12-28 20:55:43 +0800

    sync

--- app.go
+++ app.go
@@ -3,16 +3,28 @@ package main
 import (
 	"context"
 	"fmt"
+	"os"
+	"log"
+
+	"github.com/wailsapp/wails/v2/pkg/runtime"
 )
 
 // App struct
 type App struct {
 	ctx context.Context
+	Filename string
 }
+
 
 // NewApp creates a new App application struct
 func NewApp() *App {
-	return &App{}
+    var filename string
+    if len(os.Args) >= 2 {
+		// 获取第一个参数（os.Args[0] 是程序路径）
+		filename = os.Args[1]
+		log.Println("--> Filename in Args: ", filename)
+    }
+    return &App{Filename: filename}
 }
 
 // startup is called when the app starts. The context is saved
@@ -21,7 +33,125 @@ func (a *App) startup(ctx context.Context) {
 	a.ctx = ctx
 }
 
+func (a *App) Greet(ctl string, content string) string {
+    switch ctl {
+    case "init":
+        return a.getMdContent()
+    case "open":
+        return a.goOpenFile()
+    case "save":
+        return a.goSaveFile(content)
+    case "saveas":
+        return a.goSaveAsFile(content)
+	case "new":
+		return a.goNewFile()
+    default:
+        return "Command Error"
+    }
+}
+
 // Greet returns a greeting for the given name
-func (a *App) Greet(name string) string {
-	return fmt.Sprintf("Hello %s, It's show time!", name)
+func (a *App) getMdContent() string {
+    if a.Filename == "" {
+        log.Println("### Error\n没有指定文件名")
+        return "### Error\n没有指定文件名"
+    }
+
+    log.Println("要处理的文件是：", a.Filename)
+   
+    content, err := os.ReadFile(a.Filename)
+    if err != nil {
+        errMsg := fmt.Sprintf("### Error\n读取文件出错：%v", err)
+        log.Println(errMsg)
+        return errMsg
+    }
+
+	// log.Println("-->Content: ", string(content))
+    return string(content)
+}
+
+// goOpenFile
+func (a *App) goOpenFile() string {
+	odo := runtime.OpenDialogOptions{
+		DefaultDirectory: "",           // string
+		DefaultFilename:  "",           // string
+		Title:            "Open File",  // string
+		Filters: []runtime.FileFilter{
+			{
+				DisplayName: "Markdown Files (*.md)",
+				Pattern:     "*.md",
+			},
+		}, // []FileFilter
+	}
+
+	filename, err := runtime.OpenFileDialog(a.ctx, odo)
+	if err != nil {
+		return "### Test Message\n测试消息"
+	}
+
+	a.Filename = filename
+	content, err := os.ReadFile(filename)
+	if err != nil {
+		return "### Test Message\n测试消息"
+	}
+
+	return string(content)
+}
+
+func (a *App) goSaveFile(content string) string {
+	if a.Filename == "" {
+		sdo := runtime.SaveDialogOptions {
+			DefaultDirectory: "",           // string
+			DefaultFilename:  "nutitile.md",           // string
+			Title:            "Save File",  // string
+			Filters: []runtime.FileFilter{
+				{
+					DisplayName: "Markdown Files (*.md)",
+					Pattern:     "*.md",
+				},
+			}, // []FileFilter
+		}
+		filename, _ := runtime.SaveFileDialog(a.ctx, sdo)
+		a.Filename = filename
+	}
+
+	if a.Filename != "" {
+		err := os.WriteFile(a.Filename, []byte(content), 0644)
+		if err == nil {
+			return "OK"
+		}
+	}
+	return "Error"
+}
+
+func (a *App) goSaveAsFile(content string) string {
+	a.Filename = ""
+
+	sdo := runtime.SaveDialogOptions {
+		DefaultDirectory: "",           // string
+		DefaultFilename:  "nutitile.md",           // string
+		Title:            "Save File",  // string
+		Filters: []runtime.FileFilter{
+			{
+				DisplayName: "Markdown Files (*.md)",
+				Pattern:     "*.md",
+			},
+		}, // []FileFilter
+	}
+	
+	filename, _ := runtime.SaveFileDialog(a.ctx, sdo)
+
+	if filename != "" {
+		a.Filename = filename
+		err := os.WriteFile(a.Filename, []byte(content), 0644)
+		if err == nil {
+			return "OK"
+		}
+	}
+	return "Error"
+}
+
+func (a *App) goNewFile() string {
+	a.Filename = ""
+	return "OK"
 }
--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,108 +1,114 @@
 <script>
-  import logo from './assets/images/logo-universal.png'
-  import {Greet} from '../wailsjs/go/main/App.js'
-  import { marked } from 'marked'; // Or your markdown parser
-  import { onMount } from 'svelte';
+  import { onMount } from "svelte"
+  import Vditor from "vditor"
+  import { Greet } from "../wailsjs/go/main/App";
+  import Modal from "./Modal.svelte";
 
-  let markdownText = '# Hello World\n## Test 2\n### hello\n```bash\n ls -la\n ```';
-  let mdvalue = '';
+  let showModal = false;
+  let modalTitle = "";
+  let modalMessage = "";
 
-  $: mdvalue = marked(markdownText);
-  console.log('mdvalue:', mdvalue); // Inspect the output
-
-  // async function loadFileContent() {
-  //   try {
-  //     markdownText = await invoke('get_file_md');
-  //     console.log(markdownText);
-  //   } catch (error) {
-  //     console.error('Failed to load file content:', error);
-  //   }
-  // }
-
-  // onMount(() => {
-  //   loadFileContent();
-  // });
-
-</script>
-
-{@html mdvalue}
-
-<!-- <script lang="ts">
-  import logo from './assets/images/logo-universal.png'
-  import {Greet} from '../wailsjs/go/main/App.js'
-
-  let resultText: string = "Please enter your name below 👇"
-  let name: string
-
-  function greet(): void {
-    Greet(name).then(result => resultText = result)
+  function openModal(title, message) {
+      modalTitle = title;
+      modalMessage = message;
+      showModal = true;
   }
+
+  function handleClose() {
+      console.log("Modal closed!");
+  }
+ 
+  onMount(() => {
+     // console.log("--> value=", value)
+    const vditor = new Vditor("vditor-container", {
+        theme: "classic",
+        toolbar: [
+          {
+            hotkey: '^n',
+            name: 'new',
+            tipPosition: 's',
+            tip: '新建',
+            className: 'right',
+            icon: '<svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" fill="currentColor" viewBox="2 2 20 20"><path fill-rule="evenodd" d="M9 2.221V7H4.221a2 2 0 0 1 .365-.5L8.5 2.586A2 2 0 0 1 9 2.22ZM11 2v5a2 2 0 0 1-2 2H4v11a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2h-7Z" clip-rule="evenodd"/></svg>',
+            click () { clickNewFile(vditor) },
+          },
+          {
+            hotkey: '^o',
+            name: 'open',
+            tipPosition: 's',
+            tip: '打开文件',
+            className: 'right',
+            icon: '<svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="36" height="36" fill="currentColor" viewBox="2 2 20 20"><path fill-rule="evenodd" d="M4 4a2 2 0 0 0-2 2v12a2 2 0 0 0 .087.586l2.977-7.937A1 1 0 0 1 6 10h12V9a2 2 0 0 0-2-2h-4.532l-1.9-2.28A2 2 0 0 0 8.032 4H4Zm2.693 8H6.5l-3 8H18l3-8H6.693Z" clip-rule="evenodd"/></svg>',
+            click () { clickOpenFile(vditor) },
+          },
+          {
+            hotkey: '^S',
+            name: 'save',
+            tipPosition: 's',
+            tip: '保存',
+            className: 'right',
+            icon: '<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" viewBox="2 2 20 20"><path fill-rule="evenodd" d="M5 3a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V7.414A2 2 0 0 0 20.414 6L18 3.586A2 2 0 0 0 16.586 3H5Zm3 11a1 1 0 0 1 1-1h6a1 1 0 0 1 1 1v6H8v-6Zm1-7V5h6v2a1 1 0 0 1-1 1h-4a1 1 0 0 1-1-1Z" clip-rule="evenodd"/><path fill-rule="evenodd" d="M14 17h-4v-2h4v2Z" clip-rule="evenodd"/></svg>',
+            click () { clickSaveFile(vditor) },
+          },
+          {
+            hotkey: '^⇧s',
+            name: 'save as',
+            tipPosition: 's',
+            tip: '另存为...',
+            className: 'right',
+            icon: '<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" viewBox="2 2 20 20"><path fill-rule="evenodd" d="M5 3a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V7.414A2 2 0 0 0 20.414 6L18 3.586A2 2 0 0 0 16.586 3H5Zm10 11a3 3 0 1 1-6 0 3 3 0 0 1 6 0ZM8 7V5h8v2a1 1 0 0 1-1 1H9a1 1 0 0 1-1-1Z" clip-rule="evenodd"/></svg>',
+            click () { clickSaveAsFile(vditor) },
+          },
+          "|","undo","redo","|","emoji","headings","bold","italic","strike","link","|","list","ordered-list","check","outdent","indent",
+          "|","quote","line","code","inline-code","insert-before","insert-after","|","table","content-theme",
+      ],
+      toolbarConfig: {pin: true},
+      after:(() => {initMd(vditor)}),
+    })
+
+    window.runtime.WindowSetTitle("mdPad - file")
+
+  })
+
+  function initMd(vditor) {
+    Greet("init", "").then((result) => {
+      console.log("--> in greet initMd result=",result)
+      // alert("ok")
+      vditor.setValue(result)
+    });
+  }
+
+  function clickNewFile(vditor) {
+    Greet("new", "") .then(() => {
+      vditor.setValue("")
+    })
+  }
+
+  function clickOpenFile(vditor) {
+    Greet("open", "").then((result) => {
+      // console.log("--> in greet open result=",result)
+      vditor.setValue(result, true)
+    })
+  }
+
+  function clickSaveFile(vditor) {
+    Greet("save", vditor.getValue()).then((result) => {
+      // alert("Save file "+result)
+      openModal("Save file", "Save file "+result)
+    })
+  }
+
+  function clickSaveAsFile(vditor) {
+    Greet("saveas", vditor.getValue()).then((result) => {
+      alert("Save as file "+result)
+    })
+  }
+
 </script>
+
 
 <main>
-  <img alt="Wails logo" id="logo" src="{logo}">
-  <div class="result" id="result">{resultText}</div>
-  <div class="input-box" id="input">
-    <input autocomplete="off" bind:value={name} class="input" id="name" type="text"/>
-    <button class="btn" on:click={greet}>Greet</button>
-  </div>
+  <div id="vditor-container"></div>
 </main>
 
-<style>
-
-  #logo {
-    display: block;
-    width: 50%;
-    height: 50%;
-    margin: auto;
-    padding: 10% 0 0;
-    background-position: center;
-    background-repeat: no-repeat;
-    background-size: 100% 100%;
-    background-origin: content-box;
-  }
-
-  .result {
-    height: 20px;
-    line-height: 20px;
-    margin: 1.5rem auto;
-  }
-
-  .input-box .btn {
-    width: 60px;
-    height: 30px;
-    line-height: 30px;
-    border-radius: 3px;
-    border: none;
-    margin: 0 0 0 20px;
-    padding: 0 8px;
-    cursor: pointer;
-  }
-
-  .input-box .btn:hover {
-    background-image: linear-gradient(to top, #cfd9df 0%, #e2ebf0 100%);
-    color: #333333;
-  }
-
-  .input-box .input {
-    border: none;
-    border-radius: 3px;
-    outline: none;
-    height: 30px;
-    line-height: 30px;
-    padding: 0 10px;
-    background-color: rgba(240, 240, 240, 1);
-    -webkit-font-smoothing: antialiased;
-  }
-
-  .input-box .input:hover {
-    border: none;
-    background-color: rgba(255, 255, 255, 1);
-  }
-
-  .input-box .input:focus {
-    border: none;
-    background-color: rgba(255, 255, 255, 1);
-  }
-
-</style> -->
+<Modal isOpen={showModal} title={modalTitle} message={modalMessage} onClose={handleClose} />
